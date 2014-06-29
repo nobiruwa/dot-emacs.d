27e68386b90b853cb3bdede6d05b82bbaf57683e
U-hprei\ein <ein@hprei.(none)>
2014-06-29 16:53:13 +0900

    remove web-mode.git from create-repo.sh

--- create-repo.sh
+++ create-repo.sh
@@ -12,7 +12,6 @@ git clone https://github.com/nobiruwa/yasnippet-snippets.git yasnippet-snippets.
 cd $REPO_DIR
 git clone https://github.com/senny/emacs-eclim.git emacs-eclim.git
 git clone https://github.com/daleharvey/jshint-mode.git jshint-mode.git
-git clone https://github.com/fxbois/web-mode.git web-mode.git
 git clone https://github.com/emacsmirror/cedet.git cedet.git
 cd cedet.git
 touch `find . -name Makefile`
