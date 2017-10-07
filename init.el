78c0164e415b35b84329b3d0d94636ed15eddf28
ein <ein@hpnull.localdomain>
2017-10-07 15:16:41 +0900

    use groovy-mode and gradle-mode.

--- init.el
+++ init.el
@@ -867,7 +867,7 @@ Temporarily, bind expr to the return value of emmet-expr-on-line."
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (bash-completion coffee-mode ddskk dockerfile-mode elm-mode elpy emmet-mode f flycheck flycheck-pyflakes ggtags ghc god-mode graphviz-dot-mode haskell-mode idomenu jedi js2-mode lua-mode markdown-mode navi2ch powershell swiper typescript-mode undo-tree web-mode xclip yaml-mode yasnippet)))
+    (bash-completion coffee-mode ddskk dockerfile-mode elm-mode elpy emmet-mode f flycheck flycheck-pyflakes ggtags ghc god-mode gradle-mode graphviz-dot-mode groovy-mode haskell-mode idomenu jedi js2-mode lua-mode markdown-mode navi2ch powershell swiper typescript-mode undo-tree web-mode xclip yaml-mode yasnippet)))
  '(show-paren-mode t))
 
 (custom-set-faces
