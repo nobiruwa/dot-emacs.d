4821ed2faa17d3967fb6bf68c3eb0a3e99660a5b
ein <ein@hpnull.localdomain>
2019-12-20 18:55:29 +0900

    add plantuml-mode and tidal; add plantuml-mode's configuration; add vue-mode's configuration.

--- init.el
+++ init.el
@@ -842,6 +842,14 @@ Temporarily, bind expr to the return value of emmet-expr-on-line."
 (define-key ctl-x-map "\C-r"  'reopen-file)
 
 ;;;;;;;;
+;; plantuml-mode
+;;;;;;;;
+(require 'plantuml-mode)
+(add-hook 'plantuml-mode-hook
+          (lambda ()
+            (setq plantuml-jar-path (expand-file-name "~/opt/plantuml/plantuml.jar"))))
+
+;;;;;;;;
 ;; skk
 ;;;;;;;;
 ;; skk-modeが有効になると、C-jがskk-kakutei-keyにバインドされる
@@ -903,6 +911,15 @@ Temporarily, bind expr to the return value of emmet-expr-on-line."
 ;; rxvt-unicode detects C-c C-/ as C-c C-_
 (define-key undo-tree-map (kbd "C-c C-/") 'undo-tree-redo)
 (define-key undo-tree-map (kbd "C-c C-_") 'undo-tree-redo)
+
+;;;;;;;;
+;; vue-mode
+;;;;;;;;
+(require 'vue-mode)
+(add-hook 'vue-mode-hook
+          (lambda ()
+            (setq mmm-js-mode-enter-hook (lambda () (setq syntax-ppss-table nil)))
+            (setq mmm-typescript-mode-enter-hook (lambda () (setq syntax-ppss-table nil)))))
 
 ;;;;;;;;
 ;; wdired
@@ -1066,7 +1083,7 @@ Temporarily, bind expr to the return value of emmet-expr-on-line."
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (ac-slime bash-completion browse-kill-ring ccls clang-format coffee-mode company-dict company-lsp csharp-mode ddskk dockerfile-mode elm-mode elpy emmet-mode f flycheck flycheck-pyflakes flymake god-mode gradle-mode graphviz-dot-mode groovy-mode haskell-mode howm idomenu intero jedi js2-mode lsp-java lsp-mode lsp-ui lua-mode markdown-mode navi2ch omnisharp powershell purescript-mode restclient shakespeare-mode slime swiper treemacs typescript-mode undo-tree vue-mode web-mode xclip yaml-mode yasnippet yasnippet-snippets)))
+    (ac-slime bash-completion browse-kill-ring ccls clang-format coffee-mode company-dict company-lsp csharp-mode ddskk dockerfile-mode elm-mode elpy emmet-mode f flycheck flycheck-pyflakes flymake god-mode gradle-mode graphviz-dot-mode groovy-mode haskell-mode howm idomenu intero jedi js2-mode lsp-java lsp-mode lsp-ui lua-mode markdown-mode navi2ch omnisharp plantuml-mode powershell purescript-mode restclient shakespeare-mode slime swiper tidal treemacs typescript-mode undo-tree vue-mode web-mode xclip yaml-mode yasnippet yasnippet-snippets)))
  '(safe-local-variable-values
    (quote
     ((haskell-process-use-ghci . t)
