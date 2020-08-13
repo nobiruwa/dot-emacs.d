a4706e7753614b1bab92e74add460ffbd8d49a05
ein <ein@hpnull.localdomain>
2020-08-13 15:20:47 +0900

    change download URLs.

--- site-lisp/download.sh
+++ site-lisp/download.sh
@@ -1,7 +1,7 @@
-wget -N http://www.bookshelf.jp/elc/color-grep.el
-wget -N http://www.bookshelf.jp/elc/color-moccur.el
-wget -N http://www.bookshelf.jp/elc/grep-edit.el
-wget -N http://www.bookshelf.jp/elc/moccur-edit.el
+wget -N http://www.emacswiki.org/emacs/download/color-grep.el
+wget -N http://www.emacswiki.org/emacs/download/color-moccur.el
+wget -N http://www.emacswiki.org/emacs/download/grep-edit.el
+wget -N http://www.emacswiki.org/emacs/download/moccur-edit.el
 wget -N http://www.emacswiki.org/emacs/download/java-mode-indent-annotations.el
 
 sed -i -e 's/toggle-read-only/read-only-mode/g' *.el
