9a502b493790cca170fdc576ff3a4805a009011b
ein <ein@mousenull>
2022-03-20 12:09:25 +0900

    separated customize variables from init.el into emacs-custom.el.

---
+++ emacs-custom.el
@@ -0,0 +1,17 @@
+;;;
+;; custom-set-*
+;;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(cmake-mode yasnippet-snippets yasnippet-classic-snippets yaml-mode xclip wgrep web-mode vue-mode undo-tree typescript-mode tidal solarized-theme shakespeare-mode rust-mode restclient purescript-mode powershell plantuml-mode nginx-mode navi2ch magit lua-mode lsp-ui lsp-pyright lsp-java lsp-haskell js2-mode idomenu howm groovy-mode graphviz-dot-mode gradle-mode god-mode flycheck-pyflakes emmet-mode elpy elm-mode dockerfile-mode ddskk counsel company-dict coffee-mode clang-format ccls cargo browse-kill-ring bash-completion ac-slime)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
