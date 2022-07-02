c573f7780fe7aa90bd90b744009b9e72588c168c
ein <ein@mousenull>
2022-07-02 14:03:56 +0900

    remove ac-slime from the package list.

--- emacs-custom.el
+++ emacs-custom.el
@@ -7,7 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ac-slime bash-completion browse-kill-ring cargo ccls clang-format cmake-mode coffee-mode company-dict counsel ddskk dockerfile-mode elm-mode elpy emmet-mode flycheck-pyflakes god-mode gradle-mode graphviz-dot-mode groovy-mode howm idomenu js2-mode lsp-haskell lsp-java lsp-pyright lsp-ui lua-mode magit navi2ch nginx-mode plantuml-mode powershell purescript-mode restclient rust-mode shakespeare-mode solarized-theme tidal typescript-mode undo-tree vue-mode web-mode wgrep xclip yaml-mode yasnippet-classic-snippets yasnippet-snippets))
+   '(bash-completion browse-kill-ring cargo ccls clang-format cmake-mode coffee-mode company-dict counsel ddskk dockerfile-mode elm-mode elpy emmet-mode flycheck-pyflakes god-mode gradle-mode graphviz-dot-mode groovy-mode howm idomenu js2-mode lsp-haskell lsp-java lsp-pyright lsp-ui lua-mode magit navi2ch nginx-mode plantuml-mode powershell purescript-mode restclient rust-mode shakespeare-mode solarized-theme tidal typescript-mode undo-tree vue-mode web-mode wgrep xclip yaml-mode yasnippet-classic-snippets yasnippet-snippets))
  '(safe-local-variable-values
    '((eval setq-local lsp-pyright-venv-path
            (expand-file-name "~/.venvs")))))
